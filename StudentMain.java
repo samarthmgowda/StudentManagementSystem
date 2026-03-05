36bc2e2ed54cbe7c5fee7654e9d7357dee2e2b71
samarth <samarthmgowda106@gmail.com>
2026-03-05 23:15:14 +0530

    Add complete Student Management System

--- StudentMain.java
+++ StudentMain.java
@@ -42,6 +42,15 @@ class Student{
     public String getAddress(){
         return address;
     }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setDob(String dob){
+        this.dob=dob;
+    }
+    public void setAddress(String add){
+        this.address=add;
+    }
     public void setCgpa(double cgpa){
         if(cgpa<0||cgpa>10){
             System.out.println("CGPA should be in the range 0-10..!!!");
@@ -79,6 +88,15 @@ class StudentManager{
         students.add(s);
         return true;
     }
+    public boolean deleteStudent(String usn) {
+        for (Student s : students) {
+            if (s.getUsn().equals(usn)) {
+                students.remove(s);
+                return true;
+            }
+        }
+        return false;
+    }
     public Student searchStudent(String Usn){
         for(Student s : students){
             if(s.getUsn().equals(Usn)){
@@ -86,6 +104,53 @@ class StudentManager{
             }
         }
         return null;
+    }
+    public void UpdateStudent(String Usn,Scanner sc){
+        Student s = searchStudent(Usn);
+        if(s==null){
+            System.out.println("Student not found...!!!");
+        }
+        boolean updating = true;
+        while(updating){
+            System.out.println("\n1. Name" +
+                    "\n2. Dob" +
+                    "\n3. address" +
+                    "\n4. Cgpa" +
+                    "\n5. Backlogs" +
+                    "\n6. Complete updation" +
+                    "\nEnter your choice");
+            int chs=sc.nextInt();
+            sc.nextLine();
+            switch(chs){
+                case 1:
+                    System.out.print("Enter new Name : ");
+                    s.setName(sc.nextLine());
+                    break;
+                case 2:
+                    System.out.print("Enter new Dob : ");
+                    s.setDob(sc.nextLine());
+                    break;
+                case 3:
+                    System.out.print("Enter new Address : ");
+                    s.setAddress(sc.nextLine());
+                    break;
+                case 4:
+                    System.out.print("Enter new Cgpa : ");
+                    s.setCgpa(sc.nextDouble());
+                    break;
+                case 5:
+                    System.out.print("Enter new Backlogs : ");
+                    s.setBacklogs(sc.nextInt());
+                    break;
+                case 6:
+                    System.out.println("Updation Succesfull...!!!");
+                    updating=false;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
     }
     public void displayAllStudents(){
         if(students.isEmpty()){
@@ -106,8 +171,10 @@ public class StudentMain {
             System.out.println("------- Student Database Menu --------" +
                     "\n1. Add a Student " +
                     "\n2. Search a Student " +
-                    "\n3. Display Students " +
-                    "\n4. Exit" +
+                    "\n3. Display Students "+
+                    "\n4. Delete Students "+
+                    "\n5. Update" +
+                    "\n6. Exit"+
                     "\nEnter your Choice : ");
             System.out.flush();
             int ch = sc.nextInt();
@@ -147,13 +214,26 @@ public class StudentMain {
                     manager.displayAllStudents();
                     break;
                 case 4:
+                    System.out.println("Enter Usn to delete : ");
+                    String deleteusn=sc.nextLine();
+                    if(manager.deleteStudent(deleteusn)){
+                        System.out.println("Student Deleted Succesfully...!!!");
+                    }
+                    else
+                        System.out.println("Student with Usn : "+deleteusn+" not found...!!!");
+                    break;
+                case 5:
+                    System.out.println("Enter Usn of Student to update : ");
+                    String dusn=sc.nextLine();
+                    manager.UpdateStudent(dusn,sc);
+                    break;
+                case 6:
                     System.out.println("Exiting Proogram.....");
                     sc.close();
                     System.exit(0);
                     break;
                 default:
                     System.out.println("Invalid Choice....!!!");
-
             }
 
         }
